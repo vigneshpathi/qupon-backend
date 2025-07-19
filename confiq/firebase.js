dfbdad311034fd3bbc6fd99b8ddfbb8664bbdd5f
Vignesh Pathi <vignesh_pathi@epam.com>
2025-07-20 01:47:37 +0530

    firebase

--- confiq/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-require("dotenv").config();
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
