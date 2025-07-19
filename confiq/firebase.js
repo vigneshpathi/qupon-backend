1bded603fed9f8974b1d379b8a0ba72394ed25be
Vignesh Pathi <vignesh_pathi@epam.com>
2025-07-20 01:44:07 +0530

    firebase

--- confiq/firebase.js
+++ confiq/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseServiceKey.json");
+require("dotenv").config();
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
