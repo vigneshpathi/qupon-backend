8035e2785376a99e73675cc2a47bf80b3a0fb8e1
Vignesh Pathi <vignesh_pathi@epam.com>
2025-07-20 21:13:15 +0530

    latest updates

--- models/User.js
+++ models/User.js
@@ -1,18 +1,15 @@
-// models/User.js
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, unique: true },
-  dob: { type: Date, required: true },
-  userLevel: { type: Number, default: 1 },
-  prepaymentPercentage: { type: Number, default: 0 },
-  totalCouponsUploaded: { type: Number, default: 0 },
-  couponsUploadedToday: { type: Number, default: 0 }
+    userId: { type: String, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    dob: { type: Date, required: true },
+    userLevel: { type: Number, default: 1 },
+    prepaymentPercentage: { type: Number, default: 0 },
+    totalCouponsUploaded: { type: Number, default: 0 },
+    couponsUploadedToday: { type: Number, default: 0 }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
